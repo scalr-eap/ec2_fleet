9658c81193145bbe60125d58cc54b3a7c3f43b3e
Ryan <ryan.fee625@gmail.com>
2020-01-22 09:28:20 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
+resource "aws_ec2_fleet" "scalr" {
+  launch_template_config {
+    launch_template_specification {
+      launch_template_id = "lt-0052cb2ad0565b97c"
+      version            = "1"
+    }
+  }
 
+  target_capacity_specification {
+    default_target_capacity_type = "spot"
+    total_target_capacity        = 2
+  }
+}
