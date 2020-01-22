5ed45768a72aeca6123e8333421f60dab45ecec0
Ryan <ryan.fee625@gmail.com>
2020-01-22 10:12:27 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  description = "Region"
+}
+
+variable "launch_template_id" {
+  description = "Launch Template ID"
+}
+
+variable "launch_template_version" {
+  description = "Launch Template Version"
+}
+
+variable "capacity_type" {
+  description = "Capacity Type"
+}
+
+variable "total_capacity" {
+  description = "Total Capcity"
+}
